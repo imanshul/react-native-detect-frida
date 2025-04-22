b7774b4014048559245f4e9e20e782e77c59f9f2
anshul <anshul.thakur@cashe.co.in>
2025-04-22 16:22:55 +0530

    Fix issue with type

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { NativeModules, Platform } from 'react-native';
-import { RootCheckResult } from './ValueTypes';
+import type { RootCheckResult } from './ValueTypes';
 
 const LINKING_ERROR =
   `The package 'react-native-detect-frida' doesn't seem to be linked. Make sure: \n\n` +
